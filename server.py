503d99f2311eb178c8cfdbdeb5fb532ea5808509
Danny Mousa <dannymou2@gmail.com>
2026-02-19 21:20:40 +0300

    Add validation for input in emotion detection endpoint

--- server.py
+++ server.py
@@ -11,6 +11,9 @@ def renderApp():
 def emotionDetector():
     INPUT_VALUE = request.args.get("textToAnalyze")
     result = emotion_detector(INPUT_VALUE)
+    
+    if result['dominant_emotion'] == None:
+        return "Invalid text! Please try again!"
 
     return f"For the given statement, the system response is 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']} and 'sadness': {result['sadness']}. The dominant emotion is {result['dominant_emotion']}."
 
