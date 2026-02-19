54af9c3f5d0ab9560072292e3c6bc3dbfc3fc451
Danny Mousa <dannymou2@gmail.com>
2026-02-19 13:09:29 +0300

    Implement emotion detection service with Flask and structured response handling

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("My app")
+
+@app.route("/")
+def renderApp():
+    return render_template("index.html")
+
+@app.route("/emotionDetector")
+def emotionDetector():
+    INPUT_VALUE = request.args.get("textToAnalyze")
+    result = emotion_detector(INPUT_VALUE)
+
+    return f"For the given statement, the system response is 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']} and 'sadness': {result['sadness']}. The dominant emotion is {result['dominant_emotion']}."
+
+    
+if __name__ == "__main__":    app.run(debug=True)
